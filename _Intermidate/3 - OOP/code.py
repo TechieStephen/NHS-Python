85b2bc4d37b4068c5e6b1e99ceed3523ddb7bc5a
stephen <stephen>
2021-08-11 09:38:00 +0100

    oop

--- _Intermidate/3 - OOP/code.py
+++ _Intermidate/3 - OOP/code.py
@@ -1,13 +1,26 @@
-class Dog():
-    def __init__(self, name, hair_color, breed):
+class Animal:
+    def __init__(self, name, no_legs):
         self.name = name
-        self.hair_color = hair_color
-        self.breed = breed
+        self.no_legs = no_legs
 
-    def bark(self):
-        print('!!!!!!!!Barking')
+    def animal_sound(self):
+        print('the animal made a sound')
 
-    def play(self):
-        print("fetch and catch")
+class Dog(Animal):
+    def __init__(self, name, no_legs, sound):
+        Animal.__init__(self, name, no_legs)
+        self.sound = sound
 
+    def eat(self):
+        print('The dog is eating a bone')
 
+class Cat(Animal):
+    def __init__(self, name, no_legs):
+        super().__init__(name, no_legs,)
+
+dog1 = Dog('Bingo', 4, 35)
+dog1.animal_sound()
+dog1.eat()
+print(dog1.age)
+
+cat1 = Cat('fgfg', 4)
